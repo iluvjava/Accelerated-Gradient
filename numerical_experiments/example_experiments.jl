bb2859fcebd4c5699d8c4ba1893a858ec73fc2c0
Alto Legato <victor1301166040@gmail.com>
2024-12-14 02:42:18 -0800

    >w<, changes

---
+++ numerical_experiments/example_experiments.jl
@@ -0,0 +1,5 @@
+# This file is an example at how to use the generic experiment runner 
+# to execute experimenr for a list of different algorithms. 
+
+
+include("../numerical_experiments/generic_experiment_runner.jl")
--- numerical_experiments/generic_experiment_runner.jl
+++ numerical_experiments/generic_experiment_runner.jl
@@ -33,23 +33,43 @@ function repeat_experiments_for(
     initial_guess_generator::Base.Callable, 
     runnables::Vector{Base.Callable},
     true_minimum::Union{Real, Nothing}=nothing;
-    repeat_for=50, 
-)::Tuple{Vector{NTuple}, Vector{NTuple}}
-    F⁺ = true_minimum
+    repeat=50, 
+    normalize::Bool=false
+)::Dict
     C = length(runnables)
+    _to_return = Dict{Int, Tuple}()  # <-- One parameter to handle the return of values. 
     # [algorithm index][test instances][iteration k]
     _objs_list = Vector{Vector{Vector}}(undef, C)
     _grad_mapping_lists = Vector{Vector{Vector}}(undef, C)
-    
-    @showprogress for i in 1:repeat_for
-        for j in 1:C
-            _objs, _gpn = perform_collect(
-                () -> runnable[j](initial_guess_generator())
+    _estimated_min = Inf
+    for c in 1: C # for each algorithm. 
+        _algo = runnables(c)
+        _l1 = _objs_list[c] = Vector{Vector}()
+        _l2 = _grad_mapping_lists[c] = Vector{Vector}()
+        for j in 1:repeat # repeat with different initial guesses. 
+            _objs, _gpm = perform_collect(
+                () -> (
+                    _algo(initial_guess_generator())
+                ) # <-- Pass functional handler. 
             )
-
+            push!(_l1, _objs)
+            _estimated_min = min(mininum(objs), _estimated_min)
+            push!(_l2, _gpm)
         end
     end
+    if isnothing(true_minimum)
+        true_minimum = _estimated_min 
+    end
+    true_minimum = min(true_minimum, _estimated_min) - eps(Float64)
+    if normalize
+        _objs_list = [(objs .- true_minimum)/(objs[1] .- true_minimum) in _objs_list]
+    end
+    for c in 1: C
+        _to_return[c] = (_objs_list[c], _grad_mapping_lists[c])
+    end
 
-    return 
+    return _to_return
 end
 
+
+### EXAMPLE USAGE OF THE ABOVE GENERIC INTERFACE. 
